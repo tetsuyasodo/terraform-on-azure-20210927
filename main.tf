84ce4c802662b5fb99dc3d591b8dd6718b28b94c
Tetsuya Sodo <tetsuya.sodo@gmail.com>
2021-10-03 18:48:04 +0900

    added nsg

--- main.tf
+++ main.tf
@@ -46,6 +46,25 @@ resource "azurerm_public_ip" "pip" {
   allocation_method   = "Dynamic"
 }
 
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg" {
+    name                = "${var.prefix}-nsg"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "${var.prefix}-nic"
   resource_group_name = azurerm_resource_group.rg.name
@@ -57,6 +76,12 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id  ### added later
   }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "nsg-on-nic" {
+    network_interface_id      = azurerm_network_interface.nic.id
+    network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
